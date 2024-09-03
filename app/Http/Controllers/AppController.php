425a396d089e3b29da51669a196dfe088ae6da8f
gracias07 <bogninougracias@gmail.com>
2024-09-03 16:13:59 +0100

    template

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Departement;
+use App\Models\Employer;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
     //
     public function index(){
-        return view('dashboard');
+        $totalDepartements = Departement::all()->count();
+        $totalEmployers = Employer::all()->count();
+        $totalAdministrateurs = User::all()->count();
+        return view('dashboard', compact('totalDepartements','totalEmployers','totalAdministrateurs'));
     }
 }

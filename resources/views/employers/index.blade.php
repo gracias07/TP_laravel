3d28b6f40d9e195630a9a54d108530865e8bc400
gracias07 <bogninougracias@gmail.com>
2024-09-05 14:47:33 +0100

    a

--- resources/views/employers/index.blade.php
+++ resources/views/employers/index.blade.php
@@ -33,12 +33,12 @@
                     </select>
                 </div>
                 <div class="col-auto">
-                    <a class="btn app-btn-secondary" href="#">
+                    <a class="btn app-btn-secondary" href="{{route('employers.create')}}">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download me-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
 <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
 </svg>
-                        Download CSV
+                        Ajoutez un employé
                     </a>
                 </div>
             </div><!--//row-->
@@ -63,73 +63,29 @@
                     <table class="table app-table-hover mb-0 text-left">
                         <thead>
                             <tr>
-                                <th class="cell">Order</th>
-                                <th class="cell">Product</th>
-                                <th class="cell">Customer</th>
-                                <th class="cell">Date</th>
-                                <th class="cell">Status</th>
-                                <th class="cell">Total</th>
+                                <th class="cell">#</th>
+                                <th class="cell">Nom</th>
+                                <th class="cell">Prénom</th>
+                                <th class="cell">Email</th>
+                                <th class="cell">Contact</th>
                                 <th class="cell"></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td class="cell">#15346</td>
-                                <td class="cell"><span class="truncate">Lorem ipsum dolor sit amet eget volutpat erat</span></td>
-                                <td class="cell">John Sanders</td>
-                                <td class="cell"><span>17 Oct</span><span class="note">2:16 PM</span></td>
-                                <td class="cell"><span class="badge bg-success">Paid</span></td>
-                                <td class="cell">$259.35</td>
-                                <td class="cell"><a class="btn-sm app-btn-secondary" href="#">View</a></td>
-                            </tr>
-                            <tr>
-                                <td class="cell">#15345</td>
-                                <td class="cell"><span class="truncate">Consectetur adipiscing elit</span></td>
-                                <td class="cell">Dylan Ambrose</td>
-                                <td class="cell"><span class="cell-data">16 Oct</span><span class="note">03:16 AM</span></td>
-                                <td class="cell"><span class="badge bg-warning">Pending</span></td>
-                                <td class="cell">$96.20</td>
-                                <td class="cell"><a class="btn-sm app-btn-secondary" href="#">View</a></td>
-                            </tr>
-                            <tr>
-                                <td class="cell">#15344</td>
-                                <td class="cell"><span class="truncate">Pellentesque diam imperdiet</span></td>
-                                <td class="cell">Teresa Holland</td>
-                                <td class="cell"><span class="cell-data">16 Oct</span><span class="note">01:16 AM</span></td>
-                                <td class="cell"><span class="badge bg-success">Paid</span></td>
-                                <td class="cell">$123.00</td>
-                                <td class="cell"><a class="btn-sm app-btn-secondary" href="#">View</a></td>
-                            </tr>
 
+                            @forelse ($employers as $employer)
                             <tr>
-                                <td class="cell">#15343</td>
-                                <td class="cell"><span class="truncate">Vestibulum a accumsan lectus sed mollis ipsum</span></td>
-                                <td class="cell">Jayden Massey</td>
-                                <td class="cell"><span class="cell-data">15 Oct</span><span class="note">8:07 PM</span></td>
-                                <td class="cell"><span class="badge bg-success">Paid</span></td>
-                                <td class="cell">$199.00</td>
-                                <td class="cell"><a class="btn-sm app-btn-secondary" href="#">View</a></td>
-                            </tr>
+                                <td class="cell" colspan="6">Aucun employé ajouté</td>
 
-                            <tr>
-                                <td class="cell">#15342</td>
-                                <td class="cell"><span class="truncate">Justo feugiat neque</span></td>
-                                <td class="cell">Reina Brooks</td>
-                                <td class="cell"><span class="cell-data">12 Oct</span><span class="note">04:23 PM</span></td>
-                                <td class="cell"><span class="badge bg-danger">Cancelled</span></td>
-                                <td class="cell">$59.00</td>
-                                <td class="cell"><a class="btn-sm app-btn-secondary" href="#">View</a></td>
                             </tr>
+                            @empty
+                            <tr>
+                                <td class="cell" colspan="6">Aucun employé ajouté</td>
 
-                            <tr>
-                                <td class="cell">#15341</td>
-                                <td class="cell"><span class="truncate">Morbi vulputate lacinia neque et sollicitudin</span></td>
-                                <td class="cell">Raymond Atkins</td>
-                                <td class="cell"><span class="cell-data">11 Oct</span><span class="note">11:18 AM</span></td>
-                                <td class="cell"><span class="badge bg-success">Paid</span></td>
-                                <td class="cell">$678.26</td>
-                                <td class="cell"><a class="btn-sm app-btn-secondary" href="#">View</a></td>
                             </tr>
+                            @endforelse
+
+
 
                         </tbody>
                     </table>
